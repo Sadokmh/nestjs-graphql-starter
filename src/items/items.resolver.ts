4424a6cf3ad9a4de1c9c9e1eda1ed279212c2a43
sadok.mourad-mhiri <sadok.mourad-mhiri@proxym-it.com>
2021-08-19 17:52:23 +0200

    adding items

--- src/items/items.resolver.ts
+++ src/items/items.resolver.ts
@@ -1,4 +1,41 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { ItemType } from './item.dto';
+import { Item } from './item.interface';
+import { ItemInput } from './item.type';
+import { ItemsService } from './items.service';
 
 @Resolver()
-export class ItemsResolver {}
+export class ItemsResolver {
+
+
+    constructor(private readonly itemsService: ItemsService) { }
+
+    @Query(returns => [ItemType])
+    async items(): Promise<ItemType[]> {
+        return this.itemsService.findAll();
+    }
+
+    @Mutation(returns => ItemType)
+    async createItem(@Args('input') input: ItemInput): Promise<ItemType> {
+        return this.itemsService.create(input);
+    }
+
+    @Mutation(returns => ItemType)
+    async updateItem(
+        @Args('id') id: string,
+        @Args('input') input: ItemInput,
+    ) {
+        return this.itemsService.update(id, input as Item);
+    }
+
+    @Mutation(returns => ItemType)
+    async deleteItem(@Args('id') id: string) {
+        return this.itemsService.delete(id);
+    }
+
+    @Query(returns => String)
+    async hello() {
+        return 'hello';
+    }
+
+}
