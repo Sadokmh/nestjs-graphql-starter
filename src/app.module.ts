72e798f10eb14576ec9beecf140cb19b730e3979
sadok.mourad-mhiri <sadok.mourad-mhiri@proxym-it.com>
2021-08-19 12:37:59 +0200

    adding GraphQl & Mongoose Modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ItemsModule } from './items/items.module';
+import { MongooseModule } from '@nestjs/mongoose';
+
 
 @Module({
-  imports: [ItemsModule],
+  imports: [
+    ItemsModule, 
+    GraphQLModule.forRoot({
+    autoSchemaFile: 'schema.gql',
+    }),
+    MongooseModule.forRoot('mongodb://localhost/nestgraphql')
+],
   controllers: [AppController],
   providers: [AppService],
 })
